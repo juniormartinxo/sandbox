4aa4ccdcc4e645a606e0fecf2499166410615e76
Junior Martins <amjr.box@gmail.com>
2021-10-09 16:57:31 -0300

    Operador lógico de curto circuito

---
+++ js_vanilla/index.js
@@ -0,0 +1,29 @@
+// Operador de curto circuito
+
+// Operado &&
+a1 = true && true // t && t returns true
+a2 = true && false // t && f returns false
+a3 = false && true // f && t returns false
+a4 = false && 3 == 4 // f && f returns false
+a5 = 'Cat' && 'Dog' // t && t returns "Dog"
+a6 = false && 'Cat' // f && t returns false
+a7 = 'Cat' && false // t && f returns false
+a8 = '' && false // f && f returns ""
+a9 = false && '' // f && f returns false
+
+console.log('\n\nOperado &&')
+console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9)
+
+// Operado ||
+a1 = true || true // t && t returns true
+a2 = true || false // t && f returns false
+a3 = false || true // f && t returns false
+a4 = false || 3 == 4 // f && f returns false
+a5 = 'Cat' || 'Dog' // t && t returns "Dog"
+a6 = false || 'Cat' // f && t returns false
+a7 = 'Cat' || false // t && f returns false
+a8 = '' || false // f && f returns ""
+a9 = false || '' // f && f returns false
+
+console.log('\n\nOperado ||')
+console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9, '\n\n')
