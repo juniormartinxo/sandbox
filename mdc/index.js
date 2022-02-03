0668361366c66a747e34c368008b6767e21389ba
Junior Martins <amjr.box@gmail.com>
2022-02-03 05:59:20 -0300

    Calcula o MDC  entre dois números

--- mdc/index.js
+++ mdc/index.js
@@ -1,15 +1,27 @@
 function mdc(a, b) {
   const max = Math.max(a, b)
   const primes = primeNumbers(max)
+  let result = []
 
-  // Isto aqui ainda está errado
-  return primes
-    .filter(prime => {
-      if (isDivisible(a, prime) && isDivisible(b, prime)) {
-        return prime
+  primes.map(prime => {
+    let div_a = a / prime
+    let div_b = b / prime
+
+    if (isDivisible(a, prime) && isDivisible(b, prime)) {
+      result.push(prime)
+    }
+
+    while (isDivisible(div_a, prime) || isDivisible(div_b, prime)) {
+      if (isDivisible(div_a, prime) && isDivisible(div_b, prime)) {
+        result.push(prime)
       }
-    })
-    .reduce((acc, num) => acc * num)
+
+      div_a = div_a / prime
+      div_b = div_b / prime
+    }
+  })
+
+  return result.reduce((acc, num) => acc * num)
 }
 
 function primeNumbers(num) {
@@ -35,4 +47,7 @@ function isDivisible(a, b) {
   return a % b === 0 ? true : false
 }
 
-console.log('mdc', mdc(60, 120))
+console.log('mdc(12, 9)', mdc(12, 9)) // 3
+console.log('mdc(21, 7)', mdc(21, 7)) // 7
+console.log('mdc(258, 45)', mdc(258, 45)) // 3
+console.log('mdc(1400, 752)', mdc(1400, 752)) // 8
